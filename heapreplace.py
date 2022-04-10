e52acb893a140819fd02895c80f692269af0eef1
Raj Semil <75782208+Rajsemil@users.noreply.github.com>
2022-04-10 11:46:22 +0530

    Add files via upload

---
+++ heapreplace.py
@@ -0,0 +1,17 @@
+# Insert a heap take input from user
+# heapq is a python library
+# heapify is used to insert a data in the list
+# int is used to integer value
+# iinpt is used to take input fropm user 
+# heaprepplace is used to replace the data in the list
+import heapq
+h = []
+for i in range(0,int(input("How many take data: "))):
+    j = int(input("Enter a number: "))
+    h.append(j)
+heapq.heapify(h)
+for i in range(0,int(input("How many take data: "))):
+    k = int(input("Enter a number: "))
+    h.append(k)
+heapq.heapreplace(h,k)
+print(h)
