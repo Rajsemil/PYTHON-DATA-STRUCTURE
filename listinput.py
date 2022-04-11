72e2a9f2f68645552a9f6f3eaf38183497252b25
Raj Semil <75782208+Rajsemil@users.noreply.github.com>
2022-04-11 11:27:24 +0530

    Update listinput.py

--- listinput.py
+++ listinput.py
@@ -1,5 +1,5 @@
 a = []
-for i in range(0,int(input("How many data do you want to take: ")):
+for i in range(0,int(input("How many data do you want to take: "))):
 	i = int(input("Enter a number: "))
-	a.apppend(i)
+	a.append(i)
 print(a)
